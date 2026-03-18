79bc02ab89e66cba0afed224947f00706744754a
dhana <dhanansezhiyan4@gmail.com>
2026-03-18 11:09:51 +0530

    ex 1

---
+++ frame1.java
@@ -0,0 +1,10 @@
+ import java.awt.*;
+
+class FrameDemo {
+    public static void main(String args[]) {
+        Frame f = new Frame();
+        f.setTitle("dhana,-BCA");
+        f.setSize(500, 400);
+        f.setVisible(true);
+    }
+}
